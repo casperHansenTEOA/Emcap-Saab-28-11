b190099cba487b190c8a797347ddeffec281b06c
casper-hansen <60180409+casperHansenTheEnemyOfAustria@users.noreply.github.com>
2024-11-15 14:35:07 +0100

    added some functor improvements

--- Examples/functorUsageInTransform.cpp
+++ Examples/functorUsageInTransform.cpp
@@ -1,34 +1,25 @@
 //inclue stl algorith
 #include <algorithm>
 //inclue stl vector
+#include <functional>
 #include <vector>
 //inclue stl iostream
 #include <iostream>
 
-using namespace std;
-class Dynamic_inc {
-    private:
-        int increment;
 
-    public:
-
-        Dynamic_inc(int increment):
-        increment(increment) {
-
-        }
-
-        void operator()(int& value) {
-            value += increment;
-        }
-
-        ~Dynamic_inc() {
-            cout << "Dynamic_inc destructor" << endl;
-        }
-};
 
 int main(){
-    vector<int> v = {1, 2, 3, 4, 5};
+    std::vector<int> v = {1, 2, 3, 4, 5};
     int toAdd = 10;
-    transform(v.begin(), v.end(), v.begin(), Dynamic_inc(toAdd));
+    // increment functor
+
+    std::plus<int> add;
+
+    add(toAdd, 5);
+
+    // transform the vector
+
+
+
 
 }
